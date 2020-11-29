4b037429542ba1ade74d623e7e6079a0c2da2b1c
mbernardes19 <bernardes.matheus@outlook.com>
2020-11-29 10:46:17 -0300

    Adds ReportDataGenerator tests

--- __tests__/ReportDataGenerator.test.ts
+++ __tests__/ReportDataGenerator.test.ts
@@ -1,64 +1,117 @@
 import ReportDataGenerator from "../src/services/ReportDataGenerator";
-import UserRepository from "../src/repositories/UserRepository";
-import UserDao from "../src/daos/UserDao";
 import { connection } from '../db';
 import UserMapper from "../src/mappers/UserMapper";
 import AirgramClient from "../src/model/AirgramClient";
+import FreeUserRepository from "../src/repositories/FreeUserRepository";
+import FreeUserDao from "../src/daos/FreeUserDao";
+import StatsDao from "../src/daos/StatsDao";
+import { TelegramChatMember } from "../src/model/Telegram";
 
 jest.mock('airgram')
 jest.mock('../src/model/AirgramClient')
+jest.mock('../src/daos/FreeUserDao')
+jest.mock('../src/repositories/FreeUserRepository')
 
 let reportDataGenerator: ReportDataGenerator;
-const dao = new UserDao(connection);
+const mockedDao = new FreeUserDao(connection) as jest.Mocked<FreeUserDao>;
+const mockedStatsDao = new StatsDao(connection) as jest.Mocked<StatsDao>;
 const mapper = new UserMapper();
 const mockedAirgramClient = new AirgramClient(1485371, "662c661df7d0b41601f6cb8ae2ef35d6", "./libtdjson.dylib") as jest.Mocked<AirgramClient>
+const mockedFreeUserRepository = new FreeUserRepository(mockedDao, mapper) as jest.Mocked<FreeUserRepository>
 
 beforeEach(() => {
-    reportDataGenerator = new ReportDataGenerator(mockedAirgramClient, 123)
-    jest.resetAllMocks();
+    reportDataGenerator = new ReportDataGenerator(mockedAirgramClient, mockedFreeUserRepository, mockedStatsDao, 123)
 })
 
 describe('Report Data Generator', () => {
-    it('should return the new users when there are any', async () => {
+    it('only gets free users from channel', async () => {
+        mockedFreeUserRepository.getAllUsersId.mockImplementationOnce(() => Promise.resolve([1,2]))
+        mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}]))
+        const users = await reportDataGenerator.getUpdatedUserList()
+        expect(users).toStrictEqual([{userId:1}, {userId:2}])
+    })
+    it('returns the new users when there are any', async () => {
+        mockedFreeUserRepository.getAllUsersId.mockImplementationOnce(() => Promise.resolve([1,2,3]))
+        mockedFreeUserRepository.getAllUsersId.mockImplementationOnce(() => Promise.resolve([1,2,3,4]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}, {userId:4}]))
         await reportDataGenerator.updateUserList()
         const res = await reportDataGenerator.getEnteringUsers()
-        expect(res).toEqual([ 4 ])
+        expect(res).toEqual([ {userId: 4} ])
     })
-    it('should return an empty array there are only leaving users', async () => {
+    it('returns an empty array when there are only leaving users', async () => {
+        mockedFreeUserRepository.getAllUsersId.mockImplementation(() => Promise.resolve([1,2,3,4,5]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}, {userId:4}]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}]))
         await reportDataGenerator.updateUserList()
         const res = await reportDataGenerator.getEnteringUsers()
         expect(res).toEqual([])
     })
-    it('should return an empty array there is no change on users list', async () => {
+    it('returns an empty array there is no change on users list', async () => {
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}]))
         await reportDataGenerator.updateUserList()
         const res = await reportDataGenerator.getEnteringUsers()
         expect(res).toEqual([])
     })
-    it('should return the leaving users when there are any', async () => {
+    it('returns the leaving users when there are any', async () => {
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}, {userId: 4}]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:3}]))
         await reportDataGenerator.updateUserList()
         const res = await reportDataGenerator.getLeavingUsers()
-        expect(res).toEqual([ 2, 4 ])
+        expect(res).toEqual([ {userId: 2}, {userId: 4} ])
     })
-    it('should return an empty array there are no leaving users', async () => {
+    it('returns an empty array when there are no leaving users', async () => {
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId: 5}, {userId:3}]))
         await reportDataGenerator.updateUserList()
         const res = await reportDataGenerator.getLeavingUsers()
         expect(res).toEqual([])
     })
-    it('should return an empty array there is no change on users list', async () => {
+    it('returns an empty array when there is no change on users list', async () => {
+        mockedFreeUserRepository.getAllUsersId.mockImplementation(() => Promise.resolve([1,2,3]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}]))
         mockedAirgramClient.getChatMembersFromChannel.mockImplementationOnce(() => Promise.resolve([{userId:1}, {userId:2}, {userId:3}]))
         await reportDataGenerator.updateUserList()
         const res = await reportDataGenerator.getLeavingUsers()
         expect(res).toEqual([])
+    })
+
+    it('returns users that left in less than seven days from the channel', async () => {
+        const leavingUsers: TelegramChatMember[] = [
+            {userId: 1, joinedIn: new Date(2020, 10, 26)},
+            {userId: 2, joinedIn: new Date(2020, 10, 10)},
+            {userId: 3, joinedIn: new Date(2020, 10, 22)},
+            {userId: 4, joinedIn: new Date(2020, 10, 23)},
+            {userId: 5, joinedIn: new Date(2020, 10, 24)},
+        ]
+
+        const todayMock = new Date(2020, 10, 29);
+        const userIds = await reportDataGenerator.getLeavingUsersWithLessThanSevenDaysOfTimeInChannel(todayMock,leavingUsers);
+        expect(userIds).toStrictEqual([1, 4, 5])
+    })
+
+    it('returns users that have seven or more days in channel', async () => {
+        const leavingUsers: TelegramChatMember[] = [
+            {userId: 1, joinedIn: new Date(2020, 10, 26)},
+            {userId: 2, joinedIn: new Date(2020, 10, 10)},
+            {userId: 3, joinedIn: new Date(2020, 10, 5)},
+            {userId: 4, joinedIn: new Date(2020, 10, 28)},
+            {userId: 5, joinedIn: new Date(2020, 10, 24)},
+        ]
+
+        jest.spyOn(reportDataGenerator, 'getAllUsersTimeInChannel')
+            .mockImplementation(() => Promise.resolve(
+                [
+                    {userId: 6, daysInChannel: 2},
+                    {userId: 7, daysInChannel: 7},
+                    {userId: 8, daysInChannel: 10},
+                    {userId: 9, daysInChannel: 6},
+                ]
+            ))
+        const today = new Date(2020, 10, 29);
+        
+        const userIds = await reportDataGenerator.getUsersWithAtLeastSevenDaysOfTimeInChannel(today, leavingUsers)
+        expect(userIds).toStrictEqual([7, 8, 2, 3])
     })
 });
--- main.ts
+++ main.ts
@@ -1,12 +1,13 @@
-import { Airgram, Auth, prompt } from 'airgram'
-import { application } from 'express'
+import express from 'express'
 import { handleDisconnect, connection } from './db';
 import AirgramClient from './src/model/AirgramClient';
-import ReportDataGenerator from './src/services/ReportDataGenerator';
-import UserRepository from './src/repositories/UserRepository';
-import UserDao from './src/daos/UserDao';
 import UserMapper from './src/mappers/UserMapper';
 import ReportGenerator from './src/services/ReportGenerator';
+import ScheduleService from './src/services/ScheduleService';
+import FreeUserRepository from './src/repositories/FreeUserRepository';
+import FreeUserDao from './src/daos/FreeUserDao';
+import bodyParser from 'body-parser';
+import StatsDao from './src/daos/StatsDao';
 
 const delay = (n) => {
   n = n || 2000;
@@ -17,17 +18,52 @@ const delay = (n) => {
   });
 };
 
+const PORT = process.env.PORT || 3030
+const app = express();
+app.use(bodyParser.json());
+
+
 (async () => {
-  await handleDisconnect();
+  await handleDisconnect()
+  const scheduleService = new ScheduleService();
   const airgramClient = new AirgramClient(1485371, "662c661df7d0b41601f6cb8ae2ef35d6", "./libtdjson.dylib")
-  const reportGenerator = new ReportGenerator(airgramClient, 1386340547, 1386340547);
+  const userDao = new FreeUserDao(connection);
+  const statsDao = new StatsDao(connection);
+
+  const userMapper = new UserMapper();
+  const freeUserRepository = new FreeUserRepository(userDao, userMapper)
+  const reportGenerator = new ReportGenerator(airgramClient, freeUserRepository, statsDao, 1386340547, 1386340547);
+  console.log('START')
+
+app.post('/bot', async (req, res) => {
+  console.log(req.body);
+  await statsDao.addBotConversationStats({
+    constructor: { name: 'RowDataPacket' },
+    id: 0,
+    type: 'BOT_CONVERSATION',
+    userId: req.body.chatId,
+    finished: req.body.finished ? 'S' : 'N',
+    reason: req.body.reason
+  })
+})
   await reportGenerator.start();
-  console.log('COMECOU')
-  await delay(60000);
-  await reportGenerator.sendEnteringAndLeavingUsers();
-  console.log('FINALIZOU')
+  await delay(30000);
+  await reportGenerator.sendReport();
+  // await reportGenerator.sendEnteringAndLeavingUsers()
+  // await reportGenerator.sendUsersWithAtLeastSevenDaysInChannel()
+  // await reportGenerator.sendLeavingUsersWithLessThanSevenDaysInChannel()
+  // scheduleService.schedule('0 0 * * *', async () => {
+  //   await reportGenerator.start();
+  //   console.log('COMECOU')
+  // })
+  // scheduleService.schedule('59 23 * * *', async () => {
+  //   await reportGenerator.sendEnteringAndLeavingUsers();
+  //   console.log('FINALIZOU')
+  // })
+  console.log('ENVIADOS')
 })()
 
-const PORT = process.env.PORT || 3030
-const app = application
+
+
+
 app.listen(PORT, () => console.log(`Rodando na porta ${PORT}`))
